b361de10d03e7dafe087fc6f5308c8695e5330f5
Brody <tbunde01@gmail.com>
2024-01-18 09:07:08 -0500

    Add relay to prefabs, update player movement network behaviour, edit build settings

--- Assets/Scripts/MenuUI/MainMenuFunctions.cs
+++ Assets/Scripts/MenuUI/MainMenuFunctions.cs
@@ -9,60 +9,6 @@ using WebSocketSharp;
 
 public class MainMenuFunctions : MonoBehaviour
 {
-    private string hostingCode;
-    private string hostingPlayerName;
-
-    [SerializeField] private GameObject hostCodeDisplay;
-    private TextMeshProUGUI hostCodeDisplaytext;
-
-    [SerializeField] private GameObject playerNameDisplay;
-    private TextMeshProUGUI playerNameDisplaytext;
-
-    [SerializeField] private Button hostButton;
-    [SerializeField] private Button joinButton;
-
-    void Start()
-    {
-       hostCodeDisplaytext = hostCodeDisplay.GetComponent<TextMeshProUGUI>();
-       playerNameDisplaytext = playerNameDisplay.GetComponent<TextMeshProUGUI>();
-    }
-
-    void Update()
-    {
-        hostCodeDisplaytext.text = "Hosting Code: "; //+ put hosting code here however we get it
-        playerNameDisplaytext.text = hostingPlayerName;
-    }
-
-
-
-    public void GetHostCode(string code)
-    {
-        hostingCode = code;
-    }
-    public void GetHostName(string name)
-    {
-        hostingPlayerName = name;
-    }
-
-    public void EnableDisableHostButton(Button button)
-    {
-        if (hostingPlayerName.IsNullOrEmpty() == false)
-        {
-            button.interactable = true;
-        } else { button.interactable = false;
-        }
-    }
-    public void EnableDisableJoinButton(Button button)
-    {
-        if (hostingPlayerName.IsNullOrEmpty() == false && hostingCode.IsNullOrEmpty() == false)
-        {
-            button.interactable = true;
-        }
-        else
-        {
-            button.interactable = false;
-        }
-    }
 
     public void LoadGame()
     {
